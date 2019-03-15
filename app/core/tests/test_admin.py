e8abda3a27c850ae858e9b75f114f3bb11bdd1ab
buraking <soer109a@stud.kea.dk>
2019-03-15 11:27:17 +0100

    Updated admin to support custom user model

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,46 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@hotmail.com',
+            password='password123'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='test@gmail.com',
+            password='password123',
+            name='Test user full name'
+        )
+
+    def test_user_listed(self):
+        """Test that users are listed"""
+        # Reverse allows you to not manually change this everywhere,
+        # as the func do it for you
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        # Check if the http response was http
+        # 200 and contains the following content
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+
+    def test_user_change_page(self):
+        """Test that the user edit page works"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        # Reverse makes: /admin/core/user/id as url
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test that the create user page works"""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
